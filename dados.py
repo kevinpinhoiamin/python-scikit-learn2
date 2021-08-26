3c16f632f48b2ec867892342b1a84676a8f8dcf0
Kevin Pinho Iamin <kiamin@1worldsync.com>
2021-08-26 15:55:40 -0300

    Continuação do curso.

--- dados.py
+++ dados.py
@@ -15,3 +15,18 @@ def carregar_acessos():
         Y.append(int(comprou))
 
     return X, Y
+
+def carregar_buscas():
+    X = []
+    Y = []
+
+    arquivo = open('busca.csv', 'r')
+    leitor = csv.reader(arquivo)
+    leitor.__next__()
+
+    for home, busca, logado, comprou in leitor:
+        dados = [int(home), busca, int(logado)]
+        X.append(dados)
+        Y.append(int(comprou))
+
+    return X, Y
